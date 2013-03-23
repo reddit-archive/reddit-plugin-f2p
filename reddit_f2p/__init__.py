c3e48c434da798c8e121e0c2c4b4941ac44d3ef2
Neil Williams <neil@reddit.com>
2013-03-22 17:07:03 -0700

    Add skeletons for the hooks.

--- reddit_f2p/__init__.py
+++ reddit_f2p/__init__.py
@@ -1,4 +1,22 @@
+from r2.lib.hooks import HookRegistrar
 from r2.lib.plugin import Plugin
+
+hooks = HookRegistrar()
+
+
+@hooks.on("reddit-request.begin")
+def on_request():
+    # TODO: determine if this is an eligible request
+    # TODO: check for user drop cooldown
+    # TODO: if not cooling down, choose an item
+    # TODO: add item to user's inventory
+    pass
+
+
+@hooks.on("js_config")
+def add_to_js_config(config):
+    # TODO: if a game event has happened, throw it in here.
+    pass
 
 
 class FreeToPlay(Plugin):
