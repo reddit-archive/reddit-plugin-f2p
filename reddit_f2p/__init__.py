bb5f66ac25271596656d7c0075f33084cc3e510a
Neil Williams <neil@reddit.com>
2013-03-22 17:23:43 -0700

    Add f2p memcaches.

--- reddit_f2p/__init__.py
+++ reddit_f2p/__init__.py
@@ -1,3 +1,5 @@
+from r2.lib.cache import CMemcache, MemcacheChain, LocalCache
+from r2.lib.configparse import ConfigValue
 from r2.lib.plugin import Plugin
 from r2.lib.js import Module
 
@@ -5,11 +7,25 @@ from r2.lib.js import Module
 class FreeToPlay(Plugin):
     needs_static_build = True
 
+    config = {
+        ConfigValue.tuple: [
+            "f2pcaches",
+        ],
+    }
+
     js = {
         'reddit': Module('reddit.js',
             'f2p.js',
         )
     }
+
+    def on_load(self, g):
+        # TODO: use SelfEmptyingCache for localcache if we use this in jobs
+        f2p_memcaches = CMemcache(g.f2pcaches, num_clients=g.num_mc_clients)
+        g.f2pcache = MemcacheChain((
+            LocalCache(),
+            f2p_memcaches,
+        ))
 
     def load_controllers(self):
         from reddit_f2p import f2p
