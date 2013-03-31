8139fc54e457b852892805e113b4f5261156c353
Neil Williams <neil@reddit.com>
2013-03-30 23:21:35 -0700

    Fix bad state change for scores.

--- reddit_f2p/scores.py
+++ reddit_f2p/scores.py
@@ -44,7 +44,7 @@ def incr_score(team, delta):
     score_key = 'score_%s' % team
     g.f2pcache.add(score_key, 0)
     g.f2pcache.incr(score_key, delta=delta)
-    state_changes("scores")[team] += delta
+    state_changes("status")[team + "_score"] += delta
 
 
 def _get_thing_userid(thing):
