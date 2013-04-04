c047ac040ae2586410db14669f0c6572270f91c2
Neil Williams <neil@reddit.com>
2013-04-04 13:51:22 -0700

    Disable the F2P shenanigans for now.

--- reddit_f2p/__init__.py
+++ reddit_f2p/__init__.py
@@ -1,10 +1,10 @@
-import json
+#import json
 
-import pkg_resources
+#import pkg_resources
 
 from r2.lib.configparse import ConfigValue
 from r2.lib.plugin import Plugin
-from r2.lib.js import Module, TemplateFileSource
+#from r2.lib.js import Module, TemplateFileSource
 
 
 class FreeToPlay(Plugin):
@@ -21,32 +21,32 @@ class FreeToPlay(Plugin):
         ],
     }
 
-    js = {
-        'reddit': Module('reddit.js',
-            'lib/iso8601.js',
-            'f2p/scrollupdater.js',
-            'f2p/f2p.js',
-            'f2p/utils.js',
-            'f2p/items.js',
-            TemplateFileSource('f2p/panel.html'),
-            TemplateFileSource('f2p/login-message.html'),
-            TemplateFileSource('f2p/item.html'),
-            TemplateFileSource('f2p/item-bubble.html'),
-            TemplateFileSource('f2p/scores.html'),
-            TemplateFileSource('f2p/target-overlay.html'),
-        )
-    }
+    #js = {
+        #'reddit': Module('reddit.js',
+            #'lib/iso8601.js',
+            #'f2p/scrollupdater.js',
+            #'f2p/f2p.js',
+            #'f2p/utils.js',
+            #'f2p/items.js',
+            #TemplateFileSource('f2p/panel.html'),
+            #TemplateFileSource('f2p/login-message.html'),
+            #TemplateFileSource('f2p/item.html'),
+            #TemplateFileSource('f2p/item-bubble.html'),
+            #TemplateFileSource('f2p/scores.html'),
+            #TemplateFileSource('f2p/target-overlay.html'),
+        #)
+    #}
 
-    live_config = {
-        ConfigValue.float: [
-            'drop_cooldown_mu',
-            'drop_cooldown_sigma',
-        ],
+    #live_config = {
+        #ConfigValue.float: [
+            #'drop_cooldown_mu',
+            #'drop_cooldown_sigma',
+        #],
 
-        ConfigValue.dict(str, int): [
-            'f2p_rarity_weights',
-        ],
-    }
+        #ConfigValue.dict(str, int): [
+            #'f2p_rarity_weights',
+        #],
+    #}
 
     def declare_queues(self, queues):
         # imported here so we don't depend on pyx files at import time
@@ -68,24 +68,24 @@ class FreeToPlay(Plugin):
         ))
         g.cache_chains.update(f2p=g.f2pcache)
 
-        compendium = pkg_resources.resource_stream(__name__,
-                                                   "data/compendium.json")
-        g.f2pitems = json.load(compendium)
-        for kind, data in g.f2pitems.iteritems():
-            data["kind"] = kind
+        #compendium = pkg_resources.resource_stream(__name__,
+                                                   #"data/compendium.json")
+        #g.f2pitems = json.load(compendium)
+        #for kind, data in g.f2pitems.iteritems():
+            #data["kind"] = kind
 
     def add_routes(self, mc):
-        mc('/f2p/gamelog', controller='gamelog', action='listing')
-        mc('/api/f2p/:action', controller='freetoplayapi')
+        #mc('/f2p/gamelog', controller='gamelog', action='listing')
+        #mc('/api/f2p/:action', controller='freetoplayapi')
         mc('/f2p/steam/:action', controller='steam', action='start')
 
     def load_controllers(self):
-        from r2.lib.pages import Reddit
-        Reddit.extra_stylesheets.append('f2p.less')
+        #from r2.lib.pages import Reddit
+        #Reddit.extra_stylesheets.append('f2p.less')
 
-        from reddit_f2p import f2p
-        f2p.hooks.register_all()
-        f2p.monkeypatch()
+        #from reddit_f2p import f2p
+        #f2p.hooks.register_all()
+        #f2p.monkeypatch()
 
         from reddit_f2p.steam import SteamController
-        from reddit_f2p.gamelog import GameLogController
+        #from reddit_f2p.gamelog import GameLogController
